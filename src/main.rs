54cf7042aefc5eedaf54361c191f417cb897d68d
ivanjermakov <ivanjermakov1@gmail.com>
2025-03-19 18:06:32 +0100

    Wine launcher

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use anyhow::Result;
+use std::{
+    path::{Path, PathBuf},
+    process::Command,
+};
+
+use anyhow::{Context, Result};
 use clap::Parser;
 
 #[derive(Parser)]
@@ -13,5 +18,58 @@ fn main() -> Result<()> {
     let cli = Cli::parse();
     println!("Hello, world!");
     println!("{}", cli.game_name);
+
+    let results = WineLauncher::match_name(&cli.game_name)?;
+    WineLauncher::run(results.first().context("no matches")?)?;
+
     Ok(())
+}
+
+trait Matcher {
+    fn match_name(name: &str) -> Result<Vec<PathBuf>>;
+}
+
+trait Executor {
+    fn run(path: &Path) -> Result<()>;
+}
+
+struct WineLauncher;
+
+impl Matcher for WineLauncher {
+    fn match_name(name: &str) -> Result<Vec<PathBuf>> {
+        let args = [
+            "-i",
+            "-t",
+            "f",
+            "-e",
+            "exe",
+            "-g",
+            &format!("*{name}*"),
+            "/D/Program Files/",
+        ];
+        let mut command = Command::new("fd");
+        command.args(args);
+        println!("{:?}", command);
+        let output = command.output()?;
+        let result = String::from_utf8_lossy(&output.stdout)
+            .lines()
+            .map(PathBuf::from)
+            .collect();
+        Ok(result)
+    }
+}
+
+impl Executor for WineLauncher {
+    fn run(path: &Path) -> Result<()> {
+        println!("running {path:?}");
+        let mut command = Command::new("wine");
+        if let Some(parent) = path.parent() {
+            command.current_dir(parent);
+        }
+        let exec_name = path.file_name().context("no file name")?;
+        command.args([exec_name]);
+        println!("spawning {command:?}");
+        command.spawn()?;
+        Ok(())
+    }
 }
