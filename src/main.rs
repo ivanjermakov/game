be29d20241a9fc8db0f02d5eba7e9655c0282d99
ivanjermakov <ivanjermakov1@gmail.com>
2025-03-19 19:03:24 +0100

    Steam launcher

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::{
+    fs::read_to_string,
     path::{Path, PathBuf},
     process::Command,
 };
 
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use clap::Parser;
+use regex::Regex;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -19,24 +21,30 @@ fn main() -> Result<()> {
     println!("Hello, world!");
     println!("{}", cli.game_name);
 
-    let results = WineLauncher::match_name(&cli.game_name)?;
-    WineLauncher::run(results.first().context("no matches")?)?;
+    if let RunStatus::Ok = WineLauncher::run(&cli.game_name)? {
+        return Ok(());
+    }
+    if let RunStatus::Ok = SteamLauncher::run(&cli.game_name)? {
+        return Ok(());
+    }
 
-    Ok(())
+    Err(anyhow!("no launchers found"))
 }
 
-trait Matcher {
-    fn match_name(name: &str) -> Result<Vec<PathBuf>>;
+enum RunStatus {
+    Ok,
+    NotFound,
 }
 
 trait Executor {
-    fn run(path: &Path) -> Result<()>;
+    fn run(name: &str) -> Result<RunStatus>;
 }
 
 struct WineLauncher;
 
-impl Matcher for WineLauncher {
-    fn match_name(name: &str) -> Result<Vec<PathBuf>> {
+impl Executor for WineLauncher {
+    fn run(name: &str) -> Result<RunStatus> {
+        let mut command = Command::new("fd");
         let args = [
             "-i",
             "-t",
@@ -47,29 +55,89 @@ impl Matcher for WineLauncher {
             &format!("*{name}*"),
             "/D/Program Files/",
         ];
-        let mut command = Command::new("fd");
         command.args(args);
         println!("{:?}", command);
         let output = command.output()?;
-        let result = String::from_utf8_lossy(&output.stdout)
+        let path = String::from_utf8_lossy(&output.stdout)
             .lines()
             .map(PathBuf::from)
-            .collect();
-        Ok(result)
+            .next();
+        println!("running {path:?}");
+        match path {
+            Some(path) => {
+                let mut command = Command::new("wine");
+                if let Some(parent) = path.parent() {
+                    command.current_dir(parent);
+                }
+                let exec_name = path.file_name().context("no file name")?;
+                command.args([exec_name]);
+                println!("spawning {command:?}");
+                command.spawn()?;
+                Ok(RunStatus::Ok)
+            }
+            None => Ok(RunStatus::NotFound),
+        }
     }
 }
 
-impl Executor for WineLauncher {
-    fn run(path: &Path) -> Result<()> {
-        println!("running {path:?}");
-        let mut command = Command::new("wine");
-        if let Some(parent) = path.parent() {
-            command.current_dir(parent);
+struct SteamLauncher;
+
+impl Executor for SteamLauncher {
+    fn run(name: &str) -> Result<RunStatus> {
+        #[derive(Debug)]
+        struct GameInfo {
+            app_id: String,
+            name: String,
         }
-        let exec_name = path.file_name().context("no file name")?;
-        command.args([exec_name]);
-        println!("spawning {command:?}");
-        command.spawn()?;
-        Ok(())
+
+        fn parse_manifest(path: &Path) -> Result<GameInfo> {
+            let content = read_to_string(path)?;
+            let app_id_line = content
+                .lines()
+                .find(|l| l.contains("\"appid\""))
+                .context("no app id")?;
+            let name_line = content
+                .lines()
+                .find(|l| l.contains("\"name\""))
+                .context("no name")?;
+            let app_id = app_id_line
+                .split("\t")
+                .last()
+                .context("no app id")?
+                .replace("\"", "");
+            let name = name_line
+                .split("\t")
+                .last()
+                .context("no name id")?
+                .replace("\"", "");
+            Ok(GameInfo { app_id, name })
+        }
+
+        let mut command = Command::new("find");
+        let args = ["/D/SteamLibrary/", "-name", "appmanifest_*"];
+        command.args(args);
+        println!("{:?}", command);
+        let output = command.output()?;
+        let manifest_files = String::from_utf8_lossy(&output.stdout)
+            .lines()
+            .map(PathBuf::from)
+            .collect::<Vec<_>>();
+        let game = manifest_files
+            .iter()
+            .map(|p| parse_manifest(p))
+            .filter_map(Result::ok)
+            .find(|g| {
+                let re = Regex::new(&format!("(?i){}", regex::escape(name))).unwrap();
+                re.is_match(&g.name)
+            });
+        match game {
+            Some(game) => {
+                let mut command = Command::new("steam");
+                command.args([format!("steam://rungameid/{}", game.app_id)]);
+                command.spawn()?;
+                Ok(RunStatus::Ok)
+            }
+            None => Ok(RunStatus::NotFound),
+        }
     }
 }
