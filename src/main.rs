f3e188851cdc8030d6f1caf723757884cf9c8cbf
ivanjermakov <ivanjermakov1@gmail.com>
2025-03-19 17:21:39 +0100

    Cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
+use anyhow::Result;
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(value_parser, help = "Game name")]
+    game_name: String,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
     println!("Hello, world!");
+    println!("{}", cli.game_name);
+    Ok(())
 }
